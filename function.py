e1e8d483fe9c044d571c508c49e995af0ebe169d
Tiger1-creator <olivierniyongabo200@gmail.com>
2025-03-22 12:22:17 +0200

    olivier

---
+++ function.py
@@ -0,0 +1,6 @@
+def multiplication():
+    print("Multiplication of two numbers")
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Multiplication of two numbers is: ", a*b)
+multiplication()
